d01eb5a66f9b4252b8fb54affa2778b8e4665dc6
TeamVirtual-Skyz <91053310+TeamVirtual-Skyz@users.noreply.github.com>
2022-09-21 23:27:37 +0700

    Update i-donasi.js

--- plugins/i-donasi.js
+++ plugins/i-donasi.js
@@ -7,14 +7,14 @@ let handler = async (m, { conn, usedPrefix }) => {
       noteMessage: {
       extendedTextMessage: {
       text: `
-╭─「 Donasi • Dana 」
+╭─「 • Donasi • 」
 │ • indosat [6285882802642]
 │ • Ovo  [6285715334760]
 │ • Dana  [6285882802642]
 │ • Qris  [Chat OWNER]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/17722223969
+│ > Ingin donasi? Wa.me/6285882802642
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
